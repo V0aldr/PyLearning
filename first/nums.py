9f2e945ac1d0f12cb9bca8e1081dc7a097b845c3
voaldr <vorobiovss@gmail.com>
2024-04-04 19:57:06 +0300

    Добавлена функция минус

--- first/nums.py
+++ first/nums.py
@@ -1,5 +1,12 @@
 def plus(a, b):
     return f"{a} + {b} = {a + b}"
 
+text = 'text'
+
+def minus(a, b):
+    return a - b
+
+
 if __name__ == '__main__':
     print(plus(2, 5))
+    print(minus(2, 5))
