e63720e573a16122df2e0c2c2ea06e67ffe82e28
voaldr <vorobiovss@gmail.com>
2024-04-08 17:23:35 +0300

    Изменён модyль trace.py Изменён мэйн

--- first/trace.py
+++ first/trace.py
@@ -1,9 +1,19 @@
 
 class Wrapped:
+    NAME = 'qwerty'
     def __init__(self, object):
         self.wrapped = object
+        self.__enigma = 0
+        self._alarm = '_'
 
     def __getattr__(self, item):
         print('getattr: ' + item)
         return getattr(self.wrapped, item)
 
+
+
+x = Wrapped('spam')
+
+print(x.__dict__.items())
+x._Wrapped__enigma = 10
+print(x.__dict__.items())
