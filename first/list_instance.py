8f66d5c6ec729a185d141bc289c0ea74689e17c9
voaldr <vorobiovss@gmail.com>
2024-04-09 15:35:53 +0300

    Added module list_instance.py

---
+++ first/list_instance.py
@@ -0,0 +1,19 @@
+class ListInstance:
+    def __init__(self):
+        self._val = '0'
+    def __attrnames(self):
+        result = ''
+        for attr in sorted(self.__dict__):
+            result += f"\t{attr} = {self.__dict__[attr]}\n"
+        return result
+
+    def __str__(self):
+        return f"\tInstance of {self.__class__.__name__}, adress {id(self)}:\n{self.__attrnames()}"
+
+
+if __name__ == '__main__':
+    x = ListInstance()
+    print(x)
+    print(f"{x.__dict__['_val']=}")
+    print(x._val)
+    print(getattr(x, '_val'))
