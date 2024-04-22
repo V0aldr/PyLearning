42adcce930e804a387338bcd1e52b09aa96c13f3
voaldr <vorobiovss@gmail.com>
2024-04-22 13:47:17 +0300

    init

---
+++ first/tracer2.py
@@ -0,0 +1,18 @@
+def tracer(func):
+    def oncall(*args):
+        oncall.calls += 1
+        print(f"call {oncall.calls} to {func.__name__}")
+
+        oncall.calls = 0
+
+    return oncall
+
+
+class C:
+
+    @tracer
+    def spam(self, a, b, c): return a + b + c
+
+x = C()
+print(x.spam(1, 2, 3))
+print(x.spam('a', 'b', 'c'))
