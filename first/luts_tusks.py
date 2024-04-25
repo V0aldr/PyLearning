5419488ffaf0a9e70630692e03ec575ab0a2d1c8
voaldr <vorobiovss@gmail.com>
2024-04-25 14:22:58 +0300

    Перевёл в перегрузку __add__

---
+++ first/luts_tusks.py
@@ -0,0 +1,42 @@
+class Adder:
+    def __init__(self, data):
+        self.data = data
+
+    def __add__(self, other):
+        if isinstance(self.data, list):
+            if isinstance(other, list):
+                return self.data.extend(other)
+            else:
+                return self.data.append(other)
+        if isinstance(self.data, dict):
+            if isinstance(other, dict):
+                self.data = self.data | other
+                return self.data
+            else:
+                # self.data['other'] = other
+                # self.data.setdefault(str(other), other)
+                return self.data.update({f"{other}": other})
+
+
+class ListAdder(Adder): pass
+
+
+class DictAdder(Adder): pass
+
+
+x = ListAdder([1])
+x + 1
+print(x.data)
+x + [1]
+print(x.data)
+
+y = DictAdder({'y': 'y'})
+print(y.data)
+y + {'x': 'x'}
+print(y.data)
+y + 1
+print(y.data)
+
+
+
+print('Ok')
